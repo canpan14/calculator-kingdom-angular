5cdd7af7df73e0886ff3e2c048c1ad3fbdb4cfbe
canpan14 <sammachlin@hotmail.com>
2017-11-21 13:31:13 -0500

    Add route to home and some basic buttons for future pages on home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AngularFontAwesomeModule,
     BsDropdownModule.forRoot()
   ],
